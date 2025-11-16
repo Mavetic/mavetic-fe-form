ed054e6d8e308acf80177201b89140ece5c789a6
marek.vele <marek.vele@codx.cz>
2025-11-16 21:55:46 +0100

    Fix components export

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/components/*/index.ts"],
   target: "ES2020",
   skipNodeModulesBundle: true,
   unbundle: true,
