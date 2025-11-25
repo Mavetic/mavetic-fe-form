183752a9d746e9dab58283773089049a7e60347d
marek.vele <marek.vele@codx.cz>
2025-11-25 20:52:49 +0100

    Fix ladle (story) for new form api and initialization

--- src/stories/decorators/formDecorator.tsx
+++ src/stories/decorators/formDecorator.tsx
@@ -1,10 +1,18 @@
 import type { Story, StoryDecorator } from "@ladle/react";
 import type { formOptions } from "@tanstack/react-form";
 import Form from "@/components/Form";
+import { type UseAppForm, useAppForm } from "@/hooks";
 
 export default (formProps: Parameters<typeof formOptions>[0]): StoryDecorator =>
-  (Component: Story) => (
-    <Form {...formProps}>
-      <Component />
-    </Form>
-  );
+  (Component: Story) => {
+    const form = useAppForm({
+      defaultValues: { test: "" },
+      onSubmit: () => {},
+    }) as UseAppForm;
+
+    return (
+      <Form form={form}>
+        <Component />
+      </Form>
+    );
+  };
